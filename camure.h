eece8dac24882262b3a0df96e9bfe8819ed62f8d
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2024-12-01 22:31:38 +0100

    feat: update to camure 0.1.1

--- camure.h
+++ camure.h
@@ -11,97 +11,97 @@ typedef struct {
   uint16_t port;
 } camure_socket_addr_ipv4_t;
 
-// Session module (https://docs.rs/camure/0.1.0/camure/session/index.html)
-/// https://docs.rs/camure/0.1.0/camure/session/struct.Coordinator.html
+// Session module (https://docs.rs/camure/0.1.1/camure/session/index.html)
+/// https://docs.rs/camure/0.1.1/camure/session/struct.Coordinator.html
 struct camure_coordinator_t;
 typedef struct camure_coordinator_t camure_coordinator_t;
-/// https://docs.rs/camure/0.1.0/camure/session/struct.Member.html
+/// https://docs.rs/camure/0.1.1/camure/session/struct.Member.html
 struct camure_member_t;
 typedef struct camure_member_t camure_member_t;
-/// https://docs.rs/camure/0.1.0/camure/session/type.GroupId.html
+/// https://docs.rs/camure/0.1.1/camure/session/type.GroupId.html
 typedef uint16_t camure_group_id_t;
 
-// Barrier module (https://docs.rs/camure/0.1.0/camure/barrier/index.html)
-/// https://docs.rs/camure/0.1.0/camure/barrier/struct.BarrierGroupCoordinator.html
+// Barrier module (https://docs.rs/camure/0.1.1/camure/barrier/index.html)
+/// https://docs.rs/camure/0.1.1/camure/barrier/struct.BarrierGroupCoordinator.html
 struct camure_barrier_group_coordinator_t;
 typedef struct camure_barrier_group_coordinator_t camure_barrier_group_coordinator_t;
-/// https://docs.rs/camure/0.1.0/camure/barrier/struct.BarrierGroupMember.html
+/// https://docs.rs/camure/0.1.1/camure/barrier/struct.BarrierGroupMember.html
 struct camure_barrier_group_member_t;
 typedef struct camure_barrier_group_member_t camure_barrier_group_member_t;
 
-// Broadcast module (https://docs.rs/camure/0.1.0/camure/broadcast/index.html)
-/// https://docs.rs/camure/0.1.0/camure/broadcast/struct.BroadcastGroupReceiver.html
+// Broadcast module (https://docs.rs/camure/0.1.1/camure/broadcast/index.html)
+/// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupReceiver.html
 struct camure_broadcast_group_receiver_t;
 typedef struct camure_broadcast_group_receiver_t camure_broadcast_group_receiver_t;
-/// https://docs.rs/camure/0.1.0/camure/broadcast/struct.BroadcastGroupSender.html
+/// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupSender.html
 struct camure_broadcast_group_sender_t;
 typedef struct camure_broadcast_group_sender_t camure_broadcast_group_sender_t;
-// /// https://docs.rs/camure/0.1.0/camure/broadcast/struct.Message.html
+// /// https://docs.rs/camure/0.1.1/camure/broadcast/struct.Message.html
 // struct camure_message_t;
 // typedef struct camure_message_t camure_message_t;
-// /// https://docs.rs/camure/0.1.0/camure/broadcast/struct.MessageReader.html
+// /// https://docs.rs/camure/0.1.1/camure/broadcast/struct.MessageReader.html
 // struct camure_message_reader_t;
 // typedef struct camure_message_reader_t camure_message_reader_t;
-// /// https://docs.rs/camure/0.1.0/camure/broadcast/struct.MessageWriter.html
+// /// https://docs.rs/camure/0.1.1/camure/broadcast/struct.MessageWriter.html
 // struct camure_message_writer_t;
 // typedef struct camure_message_writer_t camure_message_writer_t;
 
 // Coordinator functions
 /// Coordinator destructor
 void camure_coordinator_destroy(camure_coordinator_t *coordinator);
-/// https://docs.rs/camure/latest/camure/session/struct.Coordinator.html#method.start_session
+/// https://docs.rs/camure/0.1.1/camure/session/struct.Coordinator.html#method.start_session
 camure_coordinator_t *
 camure_coordinator_start_session_ipv4(const camure_socket_addr_ipv4_t *bind_addr,
                                       const camure_socket_addr_ipv4_t *multicast_addr);
 
-/// https://docs.rs/camure/latest/camure/session/struct.Coordinator.html#method.create_barrier_group
+/// https://docs.rs/camure/0.1.1/camure/session/struct.Coordinator.html#method.create_barrier_group
 camure_barrier_group_coordinator_t *
 camure_coordinator_create_barrier_group(camure_coordinator_t *coordinator, camure_group_id_t group_id);
 
-/// https://docs.rs/camure/latest/camure/session/struct.Coordinator.html#method.create_broadcast_group
+/// https://docs.rs/camure/0.1.1/camure/session/struct.Coordinator.html#method.create_broadcast_group
 camure_broadcast_group_sender_t *
 camure_coordinator_create_broadcast_group(camure_coordinator_t *coordinator, camure_group_id_t group_id);
 
 // Member functions
 void camure_member_destroy(camure_member_t *member);
-/// https://docs.rs/camure/latest/camure/session/struct.Member.html#method.join_session
+/// https://docs.rs/camure/0.1.1/camure/session/struct.Member.html#method.join_session
 camure_member_t* camure_member_join_session_ipv4(const camure_socket_addr_ipv4_t* addr);
-/// https://docs.rs/camure/latest/camure/session/struct.Member.html#method.join_barrier_group
+/// https://docs.rs/camure/0.1.1/camure/session/struct.Member.html#method.join_barrier_group
 camure_barrier_group_member_t* camure_member_join_barrier_group(camure_member_t* member, camure_group_id_t group_id);
-/// https://docs.rs/camure/latest/camure/session/struct.Member.html#method.join_broadcast_group
+/// https://docs.rs/camure/0.1.1/camure/session/struct.Member.html#method.join_broadcast_group
 camure_broadcast_group_receiver_t* camure_member_join_broadcast_group(camure_member_t* member, camure_group_id_t group_id);
 
 // BarrierGroupCoordinator functions
 void camure_barrier_group_coordinator_destroy(camure_barrier_group_coordinator_t *barrier_group);
-// https://docs.rs/camure/latest/camure/barrier/struct.BarrierGroupCoordinator.html#method.has_members
+// https://docs.rs/camure/0.1.1/camure/barrier/struct.BarrierGroupCoordinator.html#method.has_members
 bool camure_barrier_group_coordinator_has_members(const camure_barrier_group_coordinator_t *barrier_group);
-// https://docs.rs/camure/latest/camure/barrier/struct.BarrierGroupCoordinator.html#method.accept
+// https://docs.rs/camure/0.1.1/camure/barrier/struct.BarrierGroupCoordinator.html#method.accept
 bool camure_barrier_group_coordinator_accept(camure_barrier_group_coordinator_t *barrier_group, camure_socket_addr_ipv4_t *addr);
-// https://docs.rs/camure/latest/camure/barrier/struct.BarrierGroupCoordinator.html#method.try_accept
+// https://docs.rs/camure/0.1.1/camure/barrier/struct.BarrierGroupCoordinator.html#method.try_accept
 bool camure_barrier_group_coordinator_try_accept(camure_barrier_group_coordinator_t *barrier_group, camure_socket_addr_ipv4_t *addr);
-// https://docs.rs/camure/latest/camure/barrier/struct.BarrierGroupCoordinator.html#method.wait
+// https://docs.rs/camure/0.1.1/camure/barrier/struct.BarrierGroupCoordinator.html#method.wait
 bool camure_barrier_group_coordinator_wait(camure_barrier_group_coordinator_t *barrier_group);
 
 // BarrierGroupMember functions
 void camure_barrier_group_member_destroy(camure_barrier_group_member_t *barrier_group);
-// https://docs.rs/camure/latest/camure/barrier/struct.BarrierGroupMember.html#method.wait
+// https://docs.rs/camure/0.1.1/camure/barrier/struct.BarrierGroupMember.html#method.wait
 bool camure_barrier_group_member_wait(camure_barrier_group_member_t *barrier_group);
 
 // BroadcastGroupSender functions
 void camure_broadcast_group_sender_destroy(camure_broadcast_group_sender_t *sender);
-// https://docs.rs/camure/latest/camure/broadcast/struct.BroadcastGroupSender.html#method.id
+// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupSender.html#method.id
 camure_group_id_t camure_broadcast_group_sender_id(const camure_broadcast_group_sender_t *sender);
-// https://docs.rs/camure/latest/camure/broadcast/struct.BroadcastGroupSender.html#method.has_members
+// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupSender.html#method.has_members
 bool camure_broadcast_group_sender_has_members(const camure_broadcast_group_sender_t *broadcast_group);
-// https://docs.rs/camure/latest/camure/broadcast/struct.BroadcastGroupSender.html#method.accept
+// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupSender.html#method.accept
 bool camure_broadcast_group_sender_accept(camure_broadcast_group_sender_t *broadcast_group, camure_socket_addr_ipv4_t *addr);
-// https://docs.rs/camure/latest/camure/broadcast/struct.BroadcastGroupSender.html#method.try_accept
+// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupSender.html#method.try_accept
 bool camure_broadcast_group_sender_try_accept(camure_broadcast_group_sender_t *broadcast_group, camure_socket_addr_ipv4_t *addr);
-// https://docs.rs/camure/latest/camure/broadcast/struct.BroadcastGroupSender.html#method.process
+// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupSender.html#method.process
 bool camure_broadcast_group_sender_process(camure_broadcast_group_sender_t *broadcast_group);
-// https://docs.rs/camure/latest/camure/broadcast/struct.BroadcastGroupSender.html#method.try_process
+// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupSender.html#method.try_process
 bool camure_broadcast_group_sender_try_process(camure_broadcast_group_sender_t *broadcast_group);
-// https://docs.rs/camure/latest/camure/broadcast/struct.BroadcastGroupSender.html#method.wait
+// https://docs.rs/camure/0.1.1/camure/broadcast/struct.BroadcastGroupSender.html#method.wait
 bool camure_broadcast_group_sender_wait(camure_broadcast_group_sender_t *broadcast_group);
 // Broadcasts a message to all members of the broadcast group.
 //
